e724a43a6d2ecdd436ff3ef7a3ce1c7cda941234
haj81 <haj_81@hotmail.com>
2021-07-16 00:56:33 -0700

    MyAssistProject

---
+++ src/com/myassist/base/action/MyAssistEngine.java
@@ -0,0 +1,10 @@
+package com.myassist.base.action;
+
+public class MyAssistEngine {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/myassist/base/action/MyAssistHome.java
@@ -0,0 +1,9 @@
+package com.myassist.base.action;
+
+public class MyAssistHome {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hai Welcome to MyAssist"); 
+	}
+
+}
